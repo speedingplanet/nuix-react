e16b7369d759e0a1453e1b6775ef3a78eb265ea5
John Paxton <pax@speedingplanet.com>
2021-08-31 09:53:12 -0400

    Fixed missing error statement

--- src/users/UsersView.jsx
+++ src/users/UsersView.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default function UsersView() {
-  <section>
-    <div className="row">
-      <div className="col">
-        <h2>Users</h2>
+  return (
+    <section>
+      <div className="row">
+        <div className="col">
+          <h2>Users</h2>
+        </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 }
